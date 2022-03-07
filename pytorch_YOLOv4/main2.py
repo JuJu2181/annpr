23a437bd758ce4c793dddd2957d90de539e10d1c
AnjaanKhadka <njn6171@gmail.com>
2022-03-07 12:57:14 +0545

    Working for video phase1

--- pytorch_YOLOv4/main2.py
+++ pytorch_YOLOv4/main2.py
@@ -51,23 +51,126 @@ def detect_characters(m1,cv2_image,boxes):
         if img is None:
             break
         # print(img)
+        numplate_chars = []
         try:
             sized = cv2.resize(img, (m1.width, m1.height))
+            sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
+            characters = do_detect(m1, sized, 0.25, 0.4, use_cuda)
+            # allcharacters.append(characters[0])
+            for character in characters[0]:
+                x1,y1,x2,y2,_,_,_ = character
+                chars = x1,y1,x2,y2 = x1_np+int(x1*height),y1_np+int(y1*width),x1_np+int(x2*height),y1_np+int(y2*width)
+                numplate_chars.append(chars)
         except:
-            return False
-        sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
-        characters = do_detect(m1, sized, 0.25, 0.4, use_cuda)
-        numplate_chars = []
-        # allcharacters.append(characters[0])
-        for character in characters[0]:
-            x1,y1,x2,y2,_,_,_ = character
-            chars = x1,y1,x2,y2 = x1_np+int(x1*height),y1_np+int(y1*width),x1_np+int(x2*height),y1_np+int(y2*width)
-            numplate_chars.append(chars)
+            pass
         allcharacters.append(numplate_chars)
     #         # print((x1_np+x1,y1_np+y1),(x2_np+x2,y2_np+y2))
     #         cv2_image = cv2.rectangle(cv2_image,(x1_np+x1,y1_np+y1),(x1_np+x2,y1_np+y2),(255,0,0),2)
     # # cv2.imshow("Cv2_image",cv2_image) 
     return allcharacters
+
+
+
+def detect_video(YOLO_Detection,YOLO_Seperation,video_path):
+    cnn_img_dimensions = (32,32)
+    #dictionary for class names 
+    cnn_class_names = {
+        0: '0',
+        1: '१',
+        2: '२',
+        3: '३',
+        4: '४',
+        5: '५',
+        6: '६',
+        7: '७',
+        8: '८',
+        9: '९',
+        10: 'बा',
+        11: 'प',
+        12: 'च'
+    }
+
+    cnn_class_names_en = {
+            0: '0',
+            1: '1',
+            2: '2',
+            3: '3',
+            4: '4',
+            5: '5',
+            6: '6',
+            7: '7',
+            8: '8',
+            9: '9',
+            10: 'ba',
+            11: 'pa',
+            12: 'cha'
+        }
+    
+    
+    
+    cfgfile,weightfile = YOLO_Detection
+    cfgfile_sep,weightfile_sep = YOLO_Seperation
+    
+    m1 = Darknet(cfgfile)
+    m2 = Darknet(cfgfile_sep)
+
+    m3_json = open('pytorch_YOLOv4/cnn_weights/model_lenet_100.json','r')
+    loaded_m3_json = m3_json.read()
+    m3_json.close() 
+    m3 = model_from_json(loaded_m3_json)
+    m3.load_weights('pytorch_YOLOv4/cnn_weights/model_lenet_100.h5')
+
+
+    # m2.print_network()
+    # m1.print_network()
+    m1.load_weights(weightfile)
+    m2.load_weights(weightfile_sep)
+    print('Loading weights from %s... Done!' % (weightfile))
+    print('Loading weights from %s... Done!' % (weightfile_sep))
+    cap = cv2.VideoCapture(video_path)
+    ret,frame = cap.read()
+    height,width,_=frame.shape
+    out = cv2.VideoWriter('outpt.avi',cv2.VideoWriter_fourcc('M','J','P','G'), cap.get(cv2.CAP_PROP_FPS),(int(width),int(height)))
+    output_numbers = []
+    while ret:
+        boxes = detect_cv2_frame(m1,frame)
+        characters_boxes = detect_characters(m2,frame,boxes)
+        for i in range(len(boxes[0])):
+            x1,y1,x2,y2,_,_,_ = number_plate_bbox = boxes[0][i]
+            x1,y1,x2,y2 = int(x1*width), int(y1*height), int(x2*width), int(y2*height)
+            segmented_charcters = sort_tuple_in_list(characters_boxes[i])
+            output_number = ""
+            for individual_char in segmented_charcters:
+                ch_x1,ch_y1,ch_x2,ch_y2 = individual_char
+                croped_char = frame[ch_y1:ch_y2,ch_x1:ch_x2]
+                frame = cv2.rectangle(frame,(ch_x1,ch_y1),(ch_x2,ch_y2),(255,0,0),1)
+                croped_char = cv2.resize(croped_char,cnn_img_dimensions)
+                croped_char = processImage(croped_char)
+                croped_char = croped_char.reshape(1,32,32,1)
+                predict_x = m3.predict(croped_char)
+                class_of_x=np.argmax(predict_x,axis=1)
+                class_id = class_of_x[0]
+                # img1 = cv2.resize(cropped_img,(300,300))
+                # print(class_id,cnn_class_names[class_id])
+                output_number+=cnn_class_names_en[class_id]
+            frame = cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),2)
+            exists = False
+            for items in output_numbers:
+                if output_number == items:
+                    exists = True
+                    break
+            if not exists:
+                output_numbers.append(output_number)
+        print(f"Output Numbers:{output_number}")
+        out.write(frame)
+        
+        ret,frame = cap.read()
+        try:
+            frame.shape
+        except:
+            break
+    print(f"All number_plates Recognized:{output_numbers}")
+
         
 
 def detect_np_from_video(YOLO_Detection,YOLO_Seperation,video_path):
@@ -334,46 +437,45 @@ def detect_cv2_image(YOLO_Detection,YOLO_Seperation,filename):
     characters_bbox = detect_characters(m2,img,boxes)
     output_numbers = []
     for i in range(len(boxes[0])):
-            # for box in boxes[0]:
-                box = boxes[0][i]
-                x1,y1,x2,y2,cf,_,_ = box
-                x1,y1,x2,y2 = int(x1*width),int(y1*height),int(x2*width),int(y2*height)
-                img = cv2.rectangle(img,(x1,y1),(x2,y2),(0,0,255),2)
-                segmented_characters = sort_tuple_in_list(characters_bbox[i]) 
-                # segmented_characters = characters_bbox[i]
-                # print(segmented_characters)
-                output_number = ""
-                for character_bbox in segmented_characters:
-                    count_i = random.randint(1, 100000)
-                    filename = f'char_{count_i}.jpg'
-                    xx1,yy1,xx2,yy2 = character_bbox
-                    # print(characters_bbox[i])
-                    # x1,y1,x2,y2 = int(x1*width),int(y1*height),int(x2*width),int(y2*height)
-                    img = cv2.rectangle(img,(xx1,yy1),(xx2,yy2),(255,0,0),2)
-                    #?cropping for character
-                    cropped_img = img[yy1:yy2,xx1:xx2]
-                    # print(f'shape: {cropped_img.shape}')
-                    # cv2.imwrite(f'pytorch-YOLOv4/detected_characters/detected_char{count_i}.jpg',cropped_img)
-                    # print(f'character_{count_i} Saved') 
-                    # img1 = cv2.imread(f'pytorch-YOLOv4/detected_characters/detected_char{count_i}.jpg',1)
-                    img1 = cv2.resize(cropped_img,cnn_img_dimensions)
-                    # preprocessing image ww
-                    img1 = processImage(img1)
-                    # cv2.imshow("Processed Image",img)
-                    # reshape image before sending it to predictor 
-                    img1 = img1.reshape(1,32,32,1)
-                    #predict 
-                    # classIndex = int(model.predict_classes(img))
-                    #predicting using model m3 for CNN
-                    predict_x=m3.predict(img1)
-                    class_of_x=np.argmax(predict_x,axis=1)
-                    class_id = class_of_x[0]
-                    img1 = cv2.resize(cropped_img,(300,300))
-                    print(class_id,cnn_class_names[class_id])
-                    output_number+=cnn_class_names_en[class_id]
-                    # cv2.putText(img,cnn_class_names_en[class_id], (50,50),fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=3, color=(0, 255, 0),thickness=3)
-                    # cv2.imwrite(filename, img)
-                output_numbers.append(output_number)
+        box = boxes[0][i]
+        x1,y1,x2,y2,cf,_,_ = box
+        x1,y1,x2,y2 = int(x1*width),int(y1*height),int(x2*width),int(y2*height)
+        img = cv2.rectangle(img,(x1,y1),(x2,y2),(0,0,255),2)
+        segmented_characters = sort_tuple_in_list(characters_bbox[i]) 
+        # segmented_characters = characters_bbox[i]
+        # print(segmented_characters)
+        output_number = ""
+        for character_bbox in segmented_characters:
+            count_i = random.randint(1, 100000)
+            filename = f'char_{count_i}.jpg'
+            xx1,yy1,xx2,yy2 = character_bbox
+            # print(characters_bbox[i])
+            # x1,y1,x2,y2 = int(x1*width),int(y1*height),int(x2*width),int(y2*height)
+            img = cv2.rectangle(img,(xx1,yy1),(xx2,yy2),(255,0,0),2)
+            #?cropping for character
+            cropped_img = img[yy1:yy2,xx1:xx2]
+            # print(f'shape: {cropped_img.shape}')
+            # cv2.imwrite(f'pytorch-YOLOv4/detected_characters/detected_char{count_i}.jpg',cropped_img)
+            # print(f'character_{count_i} Saved') 
+            # img1 = cv2.imread(f'pytorch-YOLOv4/detected_characters/detected_char{count_i}.jpg',1)
+            img1 = cv2.resize(cropped_img,cnn_img_dimensions)
+            # preprocessing image ww
+            img1 = processImage(img1)
+            # cv2.imshow("Processed Image",img)
+            # reshape image before sending it to predictor 
+            img1 = img1.reshape(1,32,32,1)
+            #predict 
+            # classIndex = int(model.predict_classes(img))
+            #predicting using model m3 for CNN
+            predict_x=m3.predict(img1)
+            class_of_x=np.argmax(predict_x,axis=1)
+            class_id = class_of_x[0]
+            img1 = cv2.resize(cropped_img,(300,300))
+            print(class_id,cnn_class_names[class_id])
+            output_number+=cnn_class_names_en[class_id]
+            # cv2.putText(img,cnn_class_names_en[class_id], (50,50),fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=3, color=(0, 255, 0),thickness=3)
+            # cv2.imwrite(filename, img)
+        output_numbers.append(output_number)
     print(f"Output Numbers: {output_numbers}")
     cv2.imwrite(f'media\detected_images\\{original_filename}', img)
     # cv2.imshow("Detected",img)
@@ -409,7 +511,7 @@ def main_annpr_detector(detector,filename):
         print(f'File: {filename}')
         output_numbers = detect_cv2_image(YOLO_detection, YOLO_seperation, filename)
     elif detector == 'video':
-        output_numbers = detect_np_from_video(YOLO_detection, YOLO_seperation, filename)
+        output_numbers = detect_video(YOLO_detection, YOLO_seperation, filename)
     else:
         print("Invalid Option!")
     end_time = time.time() 
